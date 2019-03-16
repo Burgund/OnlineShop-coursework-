7203a2126f3c894ae4b7eaa25fb6c8300f3f1844
Burgund <maciej_burchardt@wp.pl>
2019-03-16 15:13:23 +0100

    established connection between scripts.js and ASP.NET API

--- web/scripts.js
+++ web/scripts.js
@@ -1,7 +1,13 @@
 const app = document.getElementById('root')
 const container = document.createElement('div')
+//test ---------------------------
+const name1 = document.getElementById('name1')
+const price1 = document.getElementById('price1')
+//--------------------------------
 
 app.appendChild(container)
+
+var index = 0
 
 var request = new XMLHttpRequest()
 request.open('GET', 'http://localhost:52269/api/products/5', true)
@@ -9,6 +15,7 @@ request.onload = function() {
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
     data.forEach(product => {
+      index = index + 1
       const card = document.createElement('div')
       const h1 = document.createElement('h1')
       h1.textContent = product.name
@@ -19,8 +26,11 @@ request.onload = function() {
       container.appendChild(card)
       card.appendChild(h1)
       card.appendChild(p)
-      //test
-      console.log(product);
+      //test---------------------------------------------------
+      if (index == 1) {name1.textContent = product}
+      if (index == 2) {price1.textContent = product}
+      console.log(product)
+      //-------------------------------------------------------
     })
   } else {
     const errorMessage = document.createElement('marquee')
