3ce0aaad9faf23e813660dd5b2dfd85d9fcbc813
Burgund <maciej_burchardt@wp.pl>
2019-03-17 23:37:59 +0100

    cart handling interface

---
+++ api/shopAPI/shopAPI/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shopAPI.Models
+{
+    public class ProductModel
+    {
+        private string _name;
+        private double _price;
+        private int _id;
+
+        public string Name { get => _name; set => _name = value; }
+        public double Price { get => _price; set => _price = value; }
+        public int Id { get => _id; set => _id = value; }
+    }
+}
